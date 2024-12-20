626741291a06afa22681ea8476c5b8c18f3c182f
celialctnm <celia.leichtnam@gmail.com>
2024-12-20 11:56:30 +0800

    test amplify

--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,18 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>TYP</h1>
       <div className="card">
-          <h1>TYP</h1>
-          <p> To-do 1 </p>
-          <p> To-do 2 </p>
-          <p> To-do 3 </p>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
       </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
