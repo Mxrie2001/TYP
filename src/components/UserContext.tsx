14988ac70d0e36c7b396eb1da4f7baa2b845af29
celialctnm <celia.leichtnam@gmail.com>
2024-12-31 10:57:45 +0800

    refresh login

--- src/components/UserContext.tsx
+++ src/components/UserContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 interface User {
     ID: number;
@@ -19,8 +19,24 @@ const UserContext = createContext<UserContextType | undefined>(undefined);
 export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [user, setUser] = useState<User | null>(null);
 
+    useEffect(() => {
+        const savedUser = localStorage.getItem('user');
+        if (savedUser) {
+            setUser(JSON.parse(savedUser));
+        }
+    }, []);
+
+    const handleSetUser = (user: User | null) => {
+        setUser(user);
+        if (user) {
+            localStorage.setItem('user', JSON.stringify(user)); // Stocker dans localStorage
+        } else {
+            localStorage.removeItem('user'); // Retirer de localStorage
+        }
+    };
+
     return (
-        <UserContext.Provider value={{ user, setUser }}>
+        <UserContext.Provider value={{ user, setUser: handleSetUser }}>
             {children}
         </UserContext.Provider>
     );
