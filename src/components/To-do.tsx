8a83283a286ca1c03eae7ae2d1f0093798d75135
PENGLaurine <129756106+PENGLaurine@users.noreply.github.com>
2024-12-31 10:48:52 +0800

    to-do

--- src/components/To-do.tsx
+++ src/components/To-do.tsx
@@ -3,6 +3,26 @@ import React, { useEffect, useState } from "react";
 import { useUser } from './UserContext';
 import { Link } from 'react-router-dom';
 import { deleteTodo } from './TodoService';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
+import Badge from 'react-bootstrap/Badge';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './To-do.css'
+
+function state2badge(state:string):string {
+    switch (state) {
+        case 'New': return 'info';
+        default: return 'secondary';
+    }
+}
+
+function prio2stack(priority:string):string {
+    switch (priority) {
+        case 'High': return "itemDanger";
+        case 'Medium': return "itemWarn";
+        default: return "item";
+    }
+}
 
 const ToDo: React.FC = () => {
     const [items, setItems] = useState<any[]>([]);
@@ -46,7 +66,16 @@ const ToDo: React.FC = () => {
     return (
         <div>
             <h1>My to do List</h1>
+            <div className="TaskResearch" >
+                <div>
+                    <label>
+                        <input type="text" id="search" name="search" width={20}/>
+                    </label>
+                </div>
+                <button>Search</button>
+            </div>
             <button><Link to="/add-todo">Ajouter une nouvelle tâche</Link></button>
+
             {loading ? (
                 <p>Loading...</p> // Message de chargement
             ) : (
@@ -55,16 +84,21 @@ const ToDo: React.FC = () => {
 
                     {items.length > 0 ? (
                         <ul>
+
                             {items.map((item, index) => (
                                 <div key={index}>
-                                    <h2>{item.Title}</h2>
-                                    <p>{item.Content}</p>
-                                    {/*<p>{item.CategoryID}</p>*/}
-                                    <p>{item.DateFinished}</p>
-                                    <p>{item.Deadline}</p>
-                                    <p>{item.Priority}</p>
-                                    <p>{item.State}</p>
-                                    <button onClick={() => handleDelete(item.ID)}>Supprimer</button>
+                                    <div className={prio2stack(item.Priority)}>
+                                        <div className="taskTitle">{item.Title}</div>
+                                        <div className="taskContent">{item.Content}</div>
+                                        {/*<p>{item.CategoryID}</p>*/}
+                                        <div className="taskFinisihed">{item.DateFinished}</div>
+                                        <Badge bg={"danger"}>{item.Deadline}</Badge>
+                                        {/*{item.Priority}*/}
+                                        <Badge bg={state2badge(item.State)}>{item.State}</Badge>
+                                        <div className="p-2 ms-auto">
+                                            <button vertical-align:middle onClick={() => handleDelete(item.ID)}>Supprimer</button>
+                                        </div>
+                                    </div>
                                 </div>
                             ))}
                         </ul>
