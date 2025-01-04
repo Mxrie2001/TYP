082c5fef6caeabc4c4cee4a1f62b14d4c474b0f1
celialctnm <celia.leichtnam@gmail.com>
2025-01-04 15:07:01 +0800

    Edit chat page design

--- src/components/CreateChat.tsx
+++ src/components/CreateChat.tsx
@@ -1,44 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import { PutCommand, ScanCommand } from '@aws-sdk/lib-dynamodb';
-import { dynamoDBClient } from '../DynamoDB.tsx';
+import { dynamoDBClient } from '../DynamoDB';
 import { useLocation } from "react-router-dom";
+import { getUserInfo } from '../InfoUserService.tsx';
 
 interface LocationState {
     userID: string;
     matchedUserID: string;
 }
 
-// Composant pour afficher et ajouter des messages
 const ChatMessages: React.FC = () => {
     const location = useLocation();
-    const { userID } = location.state as LocationState;
-    const { matchedUserID } = location.state as LocationState;
+    const { userID, matchedUserID } = location.state as LocationState;
 
     const [messages, setMessages] = useState<any[]>([]);
     const [newMessage, setNewMessage] = useState({
-        message_id: '',  // On ne définit pas ici pour éviter le vide
+        message_id: '',
         sender_id: userID,
         receiver_id: matchedUserID,
         content: '',
         timestamp: '',
     });
+    const [isLoading, setIsLoading] = useState(true); // État de chargement
+    const [, setUserFirstName] = useState<string>('Moi');
+    const [matchedFirstName, setMatchedFirstName] = useState<string>("Match");
 
-    // Récupérer tous les messages
+    const fetchUserInfo = async () => {
+        try {
+            const userInfo = await getUserInfo(userID);
+            const matchedUserInfo = await getUserInfo(matchedUserID);
+            if (userInfo) setUserFirstName(userInfo.FirstName);
+            if (matchedUserInfo) setMatchedFirstName(matchedUserInfo.FirstName);
+        } catch (error) {
+            console.error('Erreur lors de la récupération des informations utilisateur:', error);
+        } finally {
+            setIsLoading(false); // Fin du chargement
+        }
+    };
+
     const fetchMessages = async () => {
         const command = new ScanCommand({ TableName: 'ChatMessages' });
         try {
             const data = await dynamoDBClient.send(command);
-            // Filtrer les messages pour ne garder que ceux entre l'expéditeur et le destinataire
             const filteredMessages = (data.Items || []).filter((msg: any) =>
                 (msg.sender_id === userID && msg.receiver_id === matchedUserID) ||
                 (msg.sender_id === matchedUserID && msg.receiver_id === userID)
             );
-
-            // Trier les messages par timestamp dans l'ordre croissant
-            filteredMessages.sort((a: any, b: any) => {
-                return parseInt(a.timestamp) - parseInt(b.timestamp);
-            });
-
+            filteredMessages.sort((a: any, b: any) => parseInt(a.timestamp) - parseInt(b.timestamp));
             setMessages(filteredMessages);
         } catch (error) {
             console.error("Erreur lors de la récupération des messages:", error);
@@ -46,20 +54,15 @@ const ChatMessages: React.FC = () => {
     };
 
     useEffect(() => {
+        fetchUserInfo();
         fetchMessages();
-
-        const intervalId = setInterval(() => {
-            fetchMessages(); // Appel de la fonction pour actualiser les messages
-        }, 5000); // Actualise toutes les 5 secondes
-
-        return () => clearInterval(intervalId); // Nettoyage de l'intervalle
+        const intervalId = setInterval(fetchMessages, 4000);
+        return () => clearInterval(intervalId);
     }, []);
 
-    // Ajouter un nouveau message
     const addMessage = async () => {
-        const timestamp = Date.now().toString(); // Générer un nouveau timestamp
-        const message_id = timestamp; // Utiliser le timestamp comme message_id
-
+        const timestamp = Date.now().toString();
+        const message_id = timestamp;
         const command = new PutCommand({
             TableName: 'ChatMessages',
             Item: {
@@ -70,60 +73,140 @@ const ChatMessages: React.FC = () => {
                 timestamp,
             },
         });
-
         try {
             await dynamoDBClient.send(command);
-            // Réinitialiser le formulaire avec un nouvel message_id
             setNewMessage({
-                message_id: '', // Cela n'est plus nécessaire car on génère à chaque envoi
+                message_id: '',
                 sender_id: userID,
                 receiver_id: matchedUserID,
                 content: '',
                 timestamp: '',
             });
-            fetchMessages(); // Re-récupérer les messages après ajout
+            fetchMessages();
         } catch (error) {
             console.error("Erreur lors de l'ajout du message:", error);
         }
     };
 
+    if (isLoading) {
+        return (
+            <div style={styles.loadingContainer}>
+                <p style={styles.loadingText}>Loading the chat room ...</p>
+            </div>
+        );
+    }
+
     return (
-        <div style={{ padding: '20px', maxWidth: '600px', margin: '0 auto' }}>
-            <h1>Messages de chat</h1>
-            <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+        <div style={styles.container}>
+            <h1 style={styles.header}>Chat Room</h1>
+            <div style={styles.chatBox}>
                 {messages.map((msg) => (
                     <div
                         key={msg.message_id}
                         style={{
+                            ...styles.message,
                             alignSelf: msg.sender_id === userID ? 'flex-end' : 'flex-start',
-                            backgroundColor: msg.sender_id === userID ? '#f9b44C' : '#f1f1f1',
-                            borderRadius: '10px',
-                            padding: '10px',
-                            maxWidth: '70%',
-                            wordWrap: 'break-word',
+                            backgroundColor: msg.sender_id === userID ? '#f9b44c' : '#e0e0e0',
                         }}
                     >
-                        <p style={{ margin: 0 }}>{msg.content} <span style={{ fontSize: '0.8em', color: '#888' }}>(de {msg.sender_id} à {msg.receiver_id})</span></p>
+                        <p style={styles.messageContent}>
+                            <span style={styles.senderName}>
+                                {msg.sender_id === userID ? "Me" : matchedFirstName}
+                            </span>
+                            <br />
+                            {msg.content}
+                        </p>
                     </div>
                 ))}
             </div>
-
-            <h2>Envoyer un message</h2>
-            <input
-                type="text"
-                placeholder="Contenu"
-                value={newMessage.content}
-                onChange={(e) => setNewMessage({ ...newMessage, content: e.target.value })}
-                style={{ width: '100%', padding: '10px', borderRadius: '5px', border: '1px solid #ccc', marginBottom: '10px' }}
-            />
-            <button
-                onClick={addMessage}
-                style={{ padding: '10px 15px', borderRadius: '5px', border: 'none', backgroundColor: '#4CAF50', color: 'white', cursor: 'pointer' }}
-            >
-                Envoyer
-            </button>
+            <div style={styles.inputContainer}>
+                <input
+                    type="text"
+                    placeholder="Write your message ..."
+                    value={newMessage.content}
+                    onChange={(e) => setNewMessage({ ...newMessage, content: e.target.value })}
+                    style={styles.input}
+                />
+                <button onClick={addMessage} style={styles.button}>Send</button>
+            </div>
         </div>
     );
+};
+
+const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'column' as const,
+        padding: '20px',
+        maxWidth: '600px',
+        margin: '0 auto',
+        fontFamily: '"Arial", sans-serif',
+        backgroundColor: '#ffffff',
+        borderRadius: '10px',
+        marginTop: '5%',
+    },
+    header: {
+        textAlign: 'center' as const,
+        marginBottom: '20px',
+        color: '#333',
+    },
+    chatBox: {
+        display: 'flex',
+        flexDirection: 'column' as const,
+        gap: '10px',
+        maxHeight: '400px',
+        overflowY: 'auto' as const,
+        marginBottom: '20px',
+        padding: '10px',
+        backgroundColor: '#fff',
+    },
+    message: {
+        padding: '10px',
+        borderRadius: '10px',
+        maxWidth: '70%',
+        wordWrap: 'break-word' as const,
+        boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+    },
+    messageContent: {
+        margin: 0,
+        paddingLeft: '5px',
+        paddingRight: '5px',
+    },
+    senderName: {
+        fontSize: '0.9em',
+        fontWeight: 'bold' as const,
+        color: '#555',
+        marginBottom: '5px',
+    },
+    inputContainer: {
+        display: 'flex',
+        gap: '10px',
+    },
+    input: {
+        flex: 1,
+        padding: '10px',
+        borderRadius: '5px',
+        border: '1px solid #ccc',
+    },
+    button: {
+        padding: '10px 20px',
+        borderRadius: '5px',
+        border: 'none',
+        backgroundColor: '#f9b44c',
+        color: 'white',
+        cursor: 'pointer' as const,
+        fontWeight: 'bold' as const,
+    },
+    loadingContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+    },
+    loadingText: {
+        fontSize: '1.2em',
+        color: '#555',
+    },
 };
 
 export default ChatMessages;
